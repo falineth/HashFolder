c29de1889386b30c24e0c1c858fa4eac0faa6196
Ben Bassett <185055731+falineth@users.noreply.github.com>
2026-01-11 20:37:53 +1100

    Save hash details

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::env::current_dir;
 use std::error::Error;
 use std::fmt::Display;
-use std::fs::{File, read_dir};
-use std::io::{BufReader, Read, Stdout, stdout};
+use std::fs::{File, OpenOptions, read_dir};
+use std::io::{BufReader, BufWriter, Read, Stdout, stdout};
 use std::os::unix::fs::MetadataExt;
 use std::path::PathBuf;
 use std::time::{Duration, UNIX_EPOCH};
@@ -12,6 +12,8 @@ use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use crossterm::{cursor, execute};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
+
+const HASH_DATA_FILENAME: &str = "hash.json";
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 struct FileEntry {
@@ -33,11 +35,20 @@ impl Display for AbortError {
 impl Error for AbortError {}
 
 fn main() {
-    let mut data_file: Vec<FileEntry> = Vec::default();
+    let starting_dir = match current_dir() {
+        Ok(current_dir) => current_dir,
+        Err(err) => {
+            println!("{err:?}");
+            return;
+        }
+    };
+
+    let mut data_file = load_current_hash_data(&starting_dir)
+        .expect("Should be able to read hash data file if it exists");
 
     let mut out: Stdout = stdout();
 
-    let scan_result = scan_folders(&mut out, &mut data_file);
+    let scan_result = scan_folders(&mut out, &starting_dir, &mut data_file);
 
     _ = disable_raw_mode();
     println!("");
@@ -47,15 +58,59 @@ fn main() {
         Err(err) => println!("{err:?}"),
     }
 
-    _ = execute!(out, cursor::MoveToNextLine(1));
+    if let Err(err) = save_hash_data(starting_dir, data_file) {
+        println!("{err:?}");
+    }
 }
 
-fn scan_folders(out: &mut Stdout, data_file: &mut Vec<FileEntry>) -> Result<(), Box<dyn Error>> {
+fn save_hash_data(starting_dir: PathBuf, data_file: Vec<FileEntry>) -> Result<(), Box<dyn Error>> {
+    let hash_data_filename = starting_dir.join(HASH_DATA_FILENAME);
+
+    let hash_data_file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(hash_data_filename)?;
+
+    let writer = BufWriter::new(hash_data_file);
+
+    serde_json::to_writer(writer, &data_file)?;
+
+    return Ok(());
+}
+
+fn load_current_hash_data(starting_dir: &PathBuf) -> Result<Vec<FileEntry>, Box<dyn Error>> {
+    let hash_data_file = starting_dir.join(HASH_DATA_FILENAME);
+
+    if !hash_data_file.exists() {
+        return Ok(Vec::default());
+    }
+
+    if !hash_data_file.is_file() {
+        Err(AbortError::default())?;
+    }
+
+    let file = File::open(hash_data_file)?;
+
+    let mut hash_data: Vec<FileEntry> = serde_json::from_reader(file)?;
+
+    if !hash_data.is_sorted_by_key(|entry| &entry.file_name) {
+        hash_data.sort_by(|a, b| a.file_name.cmp(&b.file_name));
+    }
+
+    return Ok(hash_data);
+}
+
+fn scan_folders(
+    out: &mut Stdout,
+    starting_dir: &PathBuf,
+    data_file: &mut Vec<FileEntry>,
+) -> Result<(), Box<dyn Error>> {
     enable_raw_mode()?;
 
     let mut pending_directories_list: Vec<PathBuf> = Vec::default();
 
-    pending_directories_list.push(current_dir()?);
+    pending_directories_list.push(starting_dir.clone());
 
     loop {
         let current_directory = match pending_directories_list.pop() {
@@ -72,7 +127,7 @@ fn scan_folders(out: &mut Stdout, data_file: &mut Vec<FileEntry>) -> Result<(), 
 fn process_folder(
     out: &mut Stdout,
     current_path: PathBuf,
-    data_file: &mut Vec<FileEntry>,
+    hash_data: &mut Vec<FileEntry>,
 ) -> Result<Vec<PathBuf>, Box<dyn Error>> {
     let mut file_list: Vec<PathBuf> = Vec::default();
     let mut subdirectory_list: Vec<PathBuf> = Vec::default();
@@ -108,17 +163,39 @@ fn process_folder(
 
         let file_size = metadata.size();
 
+        let entry_position = hash_data.binary_search_by_key(&&file_name, |entry| &entry.file_name);
+
+        if let Ok(entry_position) = entry_position {
+            if let Some(entry) = hash_data.get(entry_position) {
+                if entry.file_size == file_size && entry.modified == modified {
+                    continue;
+                }
+            }
+        }
+
         let hash = hash_file(file)?;
 
         print!("{file_name} {hash}");
         execute!(out, cursor::MoveToNextLine(1))?;
 
-        data_file.push(FileEntry {
-            file_name,
-            file_size,
-            modified,
-            hash,
-        });
+        match entry_position {
+            Ok(entry_position) => {
+                if let Some(entry) = hash_data.get_mut(entry_position) {
+                    entry.hash = hash
+                }
+            }
+            Err(entry_position) => {
+                hash_data.insert(
+                    entry_position,
+                    FileEntry {
+                        file_name,
+                        file_size,
+                        modified,
+                        hash,
+                    },
+                );
+            }
+        }
     }
 
     return Ok(subdirectory_list);
